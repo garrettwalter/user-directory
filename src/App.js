c8681abfed812f8bcfc3a707bb0205fc78e19f32
Garrett Walter <gtwalter150@gmail.com>
2021-01-23 23:06:59 -0500

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { HashRouter as Router, Route} from "react-router-dom";
 
 import Discover from "./containers/Discover/Discover";
 import Search from "./containers/Search/Search";
